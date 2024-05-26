f747b565e0841806e9e13d78b591b5534820c10f
ADARSH JHA <adarshkumarjha@ADARSHs-MacBook-Air.local>
2024-05-26 17:49:51 +0530

    renamed components according to the website

--- src/App.js
+++ src/App.js
@@ -1,11 +1,11 @@
 import './App.css';
 import ProductList from './features/product-list/ProductList';
 import {Counter } from './features/counter/Counter';
-
+import Home from './pages/Home';
 function App() {
   return (
     <div className="App">
-      <ProductList></ProductList>
+      <Home></Home>
     </div>
   );
 }
--- src/features/product-list/ProductList.js
+++ src/features/product-list/ProductList.js
@@ -231,7 +231,7 @@ export default function ProductList() {
             <main className="mx-auto max-w-7xl px-4 sm:px-6 lg:px-8">
               <div className="flex items-baseline justify-between border-b border-gray-200 pb-6 pt-24">
                 <h1 className="text-4xl font-bold tracking-tight text-gray-900">
-                  New Arrivals
+                  All Products
                 </h1>
 
                 <div className="flex items-center">
@@ -373,10 +373,8 @@ export default function ProductList() {
                     {
                       //this is our product list page
                       <div className="bg-white">
-                        <div className="mx-auto max-w-2xl px-4 py-16 sm:px-6 sm:py-24 lg:max-w-7xl lg:px-8">
-                          <h2 className="text-2xl font-bold tracking-tight text-gray-900">
-                            Products
-                          </h2>
+                        <div className="mx-auto max-w-2xl px-4 py-0 sm:px-6 sm:py-0 lg:max-w-7xl lg:px-8">
+                          
 
                           <div className="mt-6 grid grid-cols-1 gap-x-6 gap-y-10 sm:grid-cols-2 lg:grid-cols-4 xl:gap-x-8">
                             {products.map((product) => (
